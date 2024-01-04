ed6234ed8a378224ab5d35b38427f6aa9d511da7
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-01-04 15:02:16 -0300

    implementing Calculator

---
+++ 03_RestWithAspNet_Calculator/RestWithAspNet/WebApplication1/Controllers/CalculatorController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid input");
+        }
+
+        private int ConvertToDecimal(string firstNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsNumeric(string secondNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
