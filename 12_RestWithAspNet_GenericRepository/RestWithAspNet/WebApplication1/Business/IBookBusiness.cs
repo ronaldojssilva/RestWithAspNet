a8379bcbecf613569208461f3d478eeb723f1c6d
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-01-10 19:14:49 -0300

    Implementing Generic repository

---
+++ 12_RestWithAspNet_GenericRepository/RestWithAspNet/WebApplication1/Business/IBookBusiness.cs
@@ -0,0 +1,13 @@
+﻿using RestWithAspNet.Model;
+
+namespace RestWithAspNet.Business
+{
+    public interface IBookBusiness
+    {
+        Book Create(Book Book);
+        Book FindById(long id); 
+        List<Book> FindAll();
+        Book Update(Book Book);
+        void Delete(long id);
+    }
+}
---
+++ 12_RestWithAspNet_GenericRepository/RestWithAspNet/WebApplication1/Business/Implementations/BookBusinessImplementation.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using RestWithAspNet.Model;
+using RestWithAspNet.Model.Context;
+using RestWithAspNet.Repository;
+using System;
+
+namespace RestWithAspNet.Business.Implementations
+{
+    public class BookBusinessImplementation : IBookBusiness
+    {
+        private readonly IBookRepository _repository;
+
+        public BookBusinessImplementation(IBookRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Book> FindAll()
+        {
+           return _repository.FindAll();
+        }
+
+        public Book FindById(long id) => _repository.FindById(id);
+
+        public Book Create(Book Book)
+        {
+            return _repository.Create(Book);
+        }
+
+        public Book Update(Book Book)
+        {
+            return _repository.Update(Book);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+       
+    }
+}
