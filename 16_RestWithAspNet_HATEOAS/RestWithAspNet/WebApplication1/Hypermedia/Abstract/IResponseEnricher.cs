51872cbdc94a38cb8c74aff021b1b75aa7547939
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-01-11 16:02:22 -0300

    Creating Filters

--- 16_RestWithAspNet_HATEOAS/RestWithAspNet/WebApplication1/Hypermedia/Abstract/IResponseEnricher.cs
+++ 16_RestWithAspNet_HATEOAS/RestWithAspNet/WebApplication1/Hypermedia/Abstract/IResponseEnricher.cs
@@ -4,7 +4,7 @@ namespace RestWithAspNet.Hypermedia.Abstract
 {
     public interface IResponseEnricher
     {
-        bool CanEnrich(ResultExecutedContext context);
-        Task Enrich(ResultExecutedContext context);
+        bool CanEnrich(ResultExecutingContext context);
+        Task Enrich(ResultExecutingContext context);
     }
 }
