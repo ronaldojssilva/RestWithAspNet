f02510dd1e92cfda361baebe0454bedb1b170888
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-01-12 15:28:19 -0300

    Implementing the Method Responsible for Generating the RefreshToken

--- 19_RestWithAspNet_Authentication/RestWithAspNet/WebApplication1/Services/Impementation/TokenService.cs
+++ 19_RestWithAspNet_Authentication/RestWithAspNet/WebApplication1/Services/Impementation/TokenService.cs
@@ -2,6 +2,7 @@
 using RestWithAspNet.Configuration;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace RestWithAspNet.Services.Impementation
@@ -32,12 +33,35 @@ namespace RestWithAspNet.Services.Impementation
 
         public string GenerateRefreshToken()
         {
-            throw new NotImplementedException();
+            var randomNumber = new byte[32];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomNumber);
+                return Convert.ToBase64String(randomNumber);
+            }
         }
 
         public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
         {
-            throw new NotImplementedException();
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.Secret)),
+                ValidateLifetime = false,
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken securityToken;
+
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out securityToken);
+            var jwtSecurityToken = securityToken as JwtSecurityToken;
+            if (jwtSecurityToken != null ||
+                !jwtSecurityToken.Header.Alg.Equals(
+                    SecurityAlgorithms.HmacSha256,
+                    StringComparison.InvariantCulture))
+                throw new SecurityTokenException("Invalid Token");
+            return principal;
         }
     }
 }
