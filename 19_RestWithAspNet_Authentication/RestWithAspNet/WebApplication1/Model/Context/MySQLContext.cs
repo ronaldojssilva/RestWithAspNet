915d6cdef1e57948e8cf5e60cc9256450e6855e8
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-01-12 10:05:39 -0300

    Adding Dependencies and Adjustments to the Database

--- 19_RestWithAspNet_Authentication/RestWithAspNet/WebApplication1/Model/Context/MySQLContext.cs
+++ 19_RestWithAspNet_Authentication/RestWithAspNet/WebApplication1/Model/Context/MySQLContext.cs
@@ -13,5 +13,6 @@ namespace RestWithAspNet.Model.Context
 
         public DbSet<Person> Persons { get; set; }
         public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
