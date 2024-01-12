032b57ff2f1a4f993d15a5ed6f2331ae1e65c68c
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-01-11 22:10:01 -0300

    Adding CORS

---
+++ 18_RestWithAspNet_CORS/RestWithAspNet/WebApplication1/Hypermedia/HyperMediaLink.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace RestWithAspNet.Hypermedia
+{
+    public class HyperMediaLink
+    {
+        public string Rel { get; set; }
+        private string href;
+        public string Href
+        {
+            get
+            {
+                object _lock = new object();
+                lock (_lock)
+                {
+                    StringBuilder sb = new StringBuilder(href);
+                    return sb.Replace("%2f","/").ToString();
+                }
+            }
+            set 
+            { 
+                href = value;
+            }
+        }// quando tem uma barra na url o .net converte / em %2f exemplo localhost:8080%2fapi
+        public string Type { get; set; }
+        public string Action { get; set; }
+    }
+}
