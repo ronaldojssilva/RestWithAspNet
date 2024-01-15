9a72844f2b1838212e364bc075bc8ed5dd802926
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-01-15 12:15:39 -0300

    Adding PATCH verb

---
+++ 20_RestWithAspNet_Authentication_PATCH/RestWithAspNet/WebApplication1/Controllers/PersonController.cs
@@ -0,0 +1,83 @@
+using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RestWithAspNet.Business;
+using RestWithAspNet.Data.VO;
+using RestWithAspNet.Hypermedia.Filter;
+using RestWithAspNet.Model;
+
+namespace WebApplication1.Controllers
+{
+    [ApiVersion("1")]
+    [ApiController]
+    [Authorize("Bearer")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+        private IPersonBusiness _personBusiness;
+
+        public PersonController(ILogger<PersonController> logger, IPersonBusiness personBusiness)
+        {
+            _logger = logger;
+            _personBusiness = personBusiness;
+        }
+
+        [HttpGet]
+        [ProducesResponseType((200), Type = typeof(List<PersonVO>))]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get()
+        {
+            return Ok(_personBusiness.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType((200), Type = typeof(PersonVO))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get(long id)
+        {
+            var person = _personBusiness.FindById(id);
+            if (person == null) { return NotFound(); }
+            return Ok(person);
+        }
+
+        [HttpPost]
+        [ProducesResponseType((200), Type = typeof(PersonVO))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Post([FromBody] PersonVO person)
+        {
+            if (person == null) { return BadRequest(); }
+            return Ok(_personBusiness.Create(person));
+        }
+
+        [HttpPut]
+        [ProducesResponseType((200), Type = typeof(PersonVO))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Put([FromBody] PersonVO person)
+        {
+            if (person == null) { return BadRequest(); }
+            return Ok(_personBusiness.Update(person));
+        }
+
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        public IActionResult delete(long id)
+        {
+            _personBusiness.Delete(id);
+            return NoContent();
+        }
+
+    }
+}
---
+++ 20_RestWithAspNet_Authentication_PATCH/RestWithAspNet/WebApplication1/Model/Book.cs
@@ -0,0 +1,21 @@
+﻿using RestWithAspNet.Model.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithAspNet.Model
+{
+    [Table("books")]
+    public class Book: BaseEntity
+    {
+        [Column("author")]
+        public string Author { get; set; }
+
+        [Column("launch_date")]
+        public DateTime launchDate { get; set; }
+
+        [Column("price")]
+        public decimal Price { get; set; }
+
+        [Column("title")]
+        public string Title { get; set; }
+    }
+}
