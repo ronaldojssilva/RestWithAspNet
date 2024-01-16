ea5891cc29235a939c672374ea8167d17758f9bc
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-01-15 21:31:20 -0300

    Trabalhando Com Arquivos

---
+++ 22_RestWithAspNet_Authentication_WorkingWithFiles/RestWithAspNet/WebApplication1/Business/IFileBusiness.cs
@@ -0,0 +1,14 @@
+﻿using RestWithAspNet.Data.VO;
+
+namespace RestWithAspNet.Business
+{
+    public interface IFileBusiness
+    {
+        public byte[] GetFile(string fileName);
+
+        public Task<FileDatailVO> SavaFileToDisk(IFormFile formFile);
+
+        public Task<List<FileDatailVO>> SavaFileToList(IList<IFormFile> files);
+
+    }
+}
---
+++ 22_RestWithAspNet_Authentication_WorkingWithFiles/RestWithAspNet/WebApplication1/Business/Implementations/FileBusiness.cs
@@ -0,0 +1,31 @@
+﻿using RestWithAspNet.Data.VO;
+
+namespace RestWithAspNet.Business.Implementations
+{
+    public class FileBusiness : IFileBusiness
+    {
+        private readonly string _basePath;
+        private readonly IHttpContextAccessor _context;
+
+        public FileBusiness(IHttpContextAccessor context)
+        {
+            _context = context;
+            _basePath = Directory.GetCurrentDirectory() + "\\UploadDir\\";
+        }
+
+        public byte[] GetFile(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FileDatailVO> SavaFileToDisk(IFormFile formFile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<FileDatailVO>> SavaFileToList(IList<IFormFile> files)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
