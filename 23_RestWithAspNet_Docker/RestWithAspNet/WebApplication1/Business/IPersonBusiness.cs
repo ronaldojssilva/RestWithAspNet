3e0cac0bfe77b3399a3545dd1110667f991bd348
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-01-16 11:42:14 -0300

    Adding Docker Support

---
+++ 23_RestWithAspNet_Docker/RestWithAspNet/WebApplication1/Business/IPersonBusiness.cs
@@ -0,0 +1,25 @@
+﻿using RestWithAspNet.Data.VO;
+using RestWithAspNet.Hypermedia.Util;
+using RestWithAspNet.Model;
+
+namespace RestWithAspNet.Business
+{
+    public interface IPersonBusiness
+    {
+        PersonVO Create(PersonVO person);
+
+        PersonVO FindById(long id); 
+
+        PagedSearchVO<PersonVO> FindWithPagedSearch(string name, string sortDirection, int pageSize, int page);
+
+        List<PersonVO> FindAll();
+
+        PersonVO Update(PersonVO person);
+
+        PersonVO Disable(long id);
+
+        void Delete(long id);
+
+        List<PersonVO> FindByName(string firstName, string lastName);
+    }
+}
---
+++ 23_RestWithAspNet_Docker/RestWithAspNet/WebApplication1/Data/Converter/Implementation/PersonConverter.cs
@@ -0,0 +1,52 @@
+﻿using RestWithAspNet.Data.Converter.Contract;
+using RestWithAspNet.Data.VO;
+using RestWithAspNet.Model;
+
+namespace RestWithAspNet.Data.Converter.Implementation
+{
+    public class PersonConverter : IParser<PersonVO, Person>, IParser<Person, PersonVO>
+    {
+        public Person Parse(PersonVO origin)
+        {
+            if (origin == null) 
+                return null;
+            return new Person
+            {
+                Id = origin.Id,
+                FirstName = origin.FirstName,
+                LastName = origin.LastName,
+                Address = origin.Address,
+                Gender = origin.Gender,
+            };
+        }
+
+        public PersonVO Parse(Person origin)
+        {
+            if (origin == null)
+                return null;
+            return new PersonVO
+            {
+                Id = origin.Id,
+                FirstName = origin.FirstName,
+                LastName = origin.LastName,
+                Address = origin.Address,
+                Gender = origin.Gender,
+            };
+        }
+
+        public List<Person> Parse(List<PersonVO> origin)
+        {
+            if (origin == null)
+                return null;
+            return origin.Select(item => Parse(item)).ToList();
+        }
+
+        public List<PersonVO> Parse(List<Person> origin)
+        {
+            if (origin == null)
+                return null;
+            return origin.Select(item => Parse(item)).ToList();
+
+        }
+    }
+}
