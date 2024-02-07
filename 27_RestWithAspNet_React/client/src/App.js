ce50c8983fbeae04a6dfb842c22e8cac34a80bd6
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-07 13:28:55 -0300

    Starting the Login Implementation

--- 27_RestWithAspNet_React/client/src/App.js
+++ 27_RestWithAspNet_React/client/src/App.js
@@ -1,22 +1,10 @@
-import React, { useState } from 'react';
-import Header from './Header';
-
-
+import React from 'react';
+import './global.css'
+import Login from './pages/Login';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function increment(){
-    setCounter(counter + 1);
-  }
-
   return ( 
-    <div>
-      <Header>
-        Counter: {counter}
-      </Header>
-      <button onClick={increment}>Add</button>
-    </div>
+    <Login></Login>
    );
 }
 
