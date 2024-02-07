0fd3d6ab0aa67504d6cb7fec583e6bd3aa008a12
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-07 12:31:01 -0300

    The Concept of the State and Immutability

--- 27_RestWithAspNet_React/client/src/App.js
+++ 27_RestWithAspNet_React/client/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 
-function App() {
-  return ( 
-    <Header>
-      Client REST - Properties
-    </Header>
-    // <Header title="Client REST Udemy"/>
 
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  function increment(){
+    setCounter(counter + 1);
+  }
+
+  return ( 
+    <div>
+      <Header>
+        Counter: {counter}
+      </Header>
+      <button onClick={increment}>Add</button>
+    </div>
    );
 }
 
