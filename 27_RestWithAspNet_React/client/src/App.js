55d1ea8ff98ecd99da7f30d19f8825b64d3c7ecc
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-07 12:13:36 -0300

    The Concept of Property and the State

--- 27_RestWithAspNet_React/client/src/App.js
+++ 27_RestWithAspNet_React/client/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Header from './Header';
 
 function App() {
   return ( 
-    <h1>Hello world</h1>
+    <Header>
+      Client REST - Properties
+    </Header>
+    // <Header title="Client REST Udemy"/>
+
    );
 }
 
