59e61043da8d39dbefdb1c77754b0dcf57dc0b31
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-07 14:40:00 -0300

    Defining the HTML Structure of the Login Screen

--- 27_RestWithAspNet_React/client/src/pages/Login/index.js
+++ 27_RestWithAspNet_React/client/src/pages/Login/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import './styles.css';
 
+import logoImage from '../../assets/logo.svg'
+import padlock from '../../assets/padlock.png'
+
 export default function Login(){
     return (
-        <h1>Welcome</h1>
+        <div className="login-container">
+            <section className="form">
+                <img src={logoImage} alt="Logo" />
+
+                <form>
+                    <h1>Acces your Account</h1>
+                    <input placeholder="Username" />
+                    <input type='password' placeholder="Password" />
+
+                    <button type="submit">Login</button>
+                </form>
+            </section>
+            <img src={padlock} alt="Login" />
+        </div>
     )
 }
