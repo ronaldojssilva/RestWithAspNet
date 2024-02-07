07ef1d3371d93557982239e1e7d2f894f6423c39
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-07 18:02:45 -0300

    Starting the Book Screen Implementation

--- 27_RestWithAspNet_React/client/src/routes.js
+++ 27_RestWithAspNet_React/client/src/routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Login from "./pages/Login";
+import Book from "./pages/Book";
 
 export default function AppRoutes(){
     return (
        <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Login/>}/>
+                <Route path="/" exact element={<Login/>}/>
+                <Route path="/book" element={<Book/>}/>
             </Routes>
        </BrowserRouter>
     );
