52b2e050971693e15db9a89b7e391a7ff4e9cb64
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-07 17:55:08 -0300

    Configuring Routes with React Router Dom

--- 27_RestWithAspNet_React/client/src/App.js
+++ 27_RestWithAspNet_React/client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './global.css'
-import Login from './pages/Login';
+
+
+import Routes from './routes';
 
 function App() {
   return ( 
-    <Login></Login>
+    <Routes/>
    );
 }
 
