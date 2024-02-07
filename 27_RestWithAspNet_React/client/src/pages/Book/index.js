5348dce3d99ffbaac9785e65faacf4474c7f19dc
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-07 18:29:08 -0300

    Implementing the Books Screen Header

--- 27_RestWithAspNet_React/client/src/pages/Book/index.js
+++ 27_RestWithAspNet_React/client/src/pages/Book/index.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import {FiPower} from "react-icons/fi";
 
 import './styles.css'
 
+import logoImage from '../../assets/logo.svg'
+
 export default function Book(){
-    return <h1>Book</h1>
+    return (
+        <div className="book-container">
+            <header>
+                <img src={logoImage} alt="Teste"/>
+                <span> Welcome, <strong>Ronaldo</strong>!</span>
+                <Link className="button" to="/book/new">Add New Book</Link>
+                <button type="button">
+                    <FiPower size={18} color="#251fc5"/>
+                </button>
+            </header>
+        </div>
+    );
 }
