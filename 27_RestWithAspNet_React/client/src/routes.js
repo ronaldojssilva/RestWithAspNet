1caf3209e530e6ea2f2862e01979cd3f545c460b
Ronaldo Silva <ronaldojssilva@gmail.com>
2024-02-08 08:30:56 -0300

    Implementing the Book Registration Screen

--- 27_RestWithAspNet_React/client/src/routes.js
+++ 27_RestWithAspNet_React/client/src/routes.js
@@ -2,14 +2,16 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Login from "./pages/Login";
-import Book from "./pages/Book";
+import Books from "./pages/Books";
+import NewBook from "./pages/NewBook";
 
 export default function AppRoutes(){
     return (
        <BrowserRouter>
             <Routes>
                 <Route path="/" exact element={<Login/>}/>
-                <Route path="/book" element={<Book/>}/>
+                <Route path="/books" element={<Books/>}/>
+                <Route path="/book/new" element={<NewBook/>}/>
             </Routes>
        </BrowserRouter>
     );
